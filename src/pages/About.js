7f47f06c4ef52fb2a1e3b8e12faaa1a02cdf39bb
Bongouade <bongouade@gmail.com>
2022-04-23 06:43:55 +0000

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation />
+            <h1>A prpopos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nulla ab deleniti reprehenderit! Sit error dolor a veritatis sint suscipit quia doloremque expedita odit fuga consequatur facilis libero reiciendis, commodi id assumenda porro aperiam ipsum dignissimos modi officiis, eveniet eligendi vero. Rerum odit ipsum distinctio optio non, placeat sequi totam aspernatur magnam quos natus voluptas ipsam recusandae, dolorum tempora? Dignissimos laboriosam, eos optio reprehenderit labore provident. Voluptates natus ipsam veniam reprehenderit, cum deserunt tempora saepe. Necessitatibus eveniet magni quod quas minus facere libero explicabo velit officiis nostrum, sit vero rerum praesentium in nulla. Hic, error vero quaerat explicabo atque tempora.</p>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque veniam omnis quia, ducimus vitae, laudantium, ut in perspiciatis sapiente maxime reprehenderit necessitatibus veritatis molestias. Animi voluptatum accusamus, inventore ipsa autem expedita laborum deleniti, nesciunt asperiores, consequuntur aliquid. Voluptates quas aperiam, nisi iusto architecto quod in vero amet, nesciunt sunt magnam, a illum iure? Ducimus accusantium possimus commodi maxime, illum tenetur harum nemo suscipit itaque debitis blanditiis, reiciendis provident beatae! Aperiam dolorum corrupti tenetur quae vel! Ducimus animi id fugiat impedit necessitatibus unde voluptas hic temporibus sequi dolore placeat, doloremque labore iste corrupti et culpa beatae voluptatum similique odit eum laudantium, debitis est consequatur aut? Eius repudiandae eveniet possimus alias nihil libero voluptate, iure vero quia neque perspiciatis cupiditate accusamus repellendus maxime omnis autem. Quam, obcaecati aliquam iusto exercitationem eius architecto! Laborum eius modi ut laudantium voluptates nam optio debitis libero fugiat suscipit perspiciatis doloremque distinctio quo, nostrum qui dolores nemo sapiente eveniet nisi itaque soluta incidunt officia repellendus. Ab, ut? Fugiat deleniti, ea tempore blanditiis, sint maxime perferendis odio natus adipisci ut reprehenderit. Quos error tenetur nam, aliquid cumque maiores eligendi iure iste accusamus saepe adipisci fugit nostrum sunt eaque. Facere ipsam sequi labore? Numquam iure minus nam minima obcaecati?</p>
+        </div>
+    );
+};
+
+export default About;
